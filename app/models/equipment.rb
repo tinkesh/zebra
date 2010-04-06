ab2c200ec0eeb084446fb85550e4e7f763b76e44
Dana Janssen <dana@Macintosh.local>
2010-04-05 18:05:58 -0600

    fix 0 calculations for equipment

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -2,14 +2,18 @@ class Equipment < ActiveRecord::Base
 
   has_and_belongs_to_many :jobs
   has_many :load_sheets
-  
+
   validates_presence_of :unit
   validates_presence_of :name
   validates_presence_of :rate
 
   def cost(job_sheet)
-    @job_sheet = JobSheet.find(job_sheet)
-    @job_sheet.time_sheets.length * self.rate
+    if self.rate
+      @job_sheet = JobSheet.find(job_sheet)
+      @job_sheet.time_sheets.length * self.rate
+    else
+      0
+    end
   end
 
 end
