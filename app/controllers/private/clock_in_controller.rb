34d0b7c9b24b7de4e36f2677be063932bd1de1ca
Dana Janssen <dana@agilestyle.com>
2010-04-25 17:11:34 -0600

    add check for params[:users] before creating clock ins

--- app/controllers/private/clock_in_controller.rb
+++ app/controllers/private/clock_in_controller.rb
@@ -17,19 +17,21 @@ class Private::ClockInController < ApplicationController
     @clock_in_time = params[:job][:'started_on(1i)'] + '-' + params[:job][:'started_on(2i)'] + '-'+ params[:job][:'started_on(3i)'] + ' ' + params[:job][:'started_on(4i)'] + ':' + params[:job][:'started_on(5i)']
     build_clocked_in_ids
 
-    params[:users].each do |user|
-      @entry = TimeEntry.find(:first, :conditions => {:user_id => user[1][:user_id], :active => nil})
-      unless @entry.blank?
-        @entry.update_attributes(user[1])
-      else
-        @entry = TimeEntry.new(user[1])
+    if params[:users]
+      params[:users].each do |user|
+        @entry = TimeEntry.find(:first, :conditions => {:user_id => user[1][:user_id], :active => nil})
+        unless @entry.blank?
+          @entry.update_attributes(user[1])
+        else
+          @entry = TimeEntry.new(user[1])
+        end
+        if @entry.active
+          @entry.clock_in = Time.parse(@clock_in_time)
+          @entry.clocked_in_at = Time.now
+          @entry.clocked_in_by = current_user.id
+        end
+        @entry.save
       end
-      if @entry.active
-        @entry.clock_in = Time.parse(@clock_in_time)
-        @entry.clocked_in_at = Time.now
-        @entry.clocked_in_by = current_user.id
-      end
-      @entry.save
     end
 
     if params[:navigate]
