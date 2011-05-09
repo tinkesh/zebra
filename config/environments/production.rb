78d74292858e8ccc07f0275015fe20fa2f3c0114
Dana Janssen <dana@agilestyle.com>
2011-05-09 08:59:08 -0600

    Install exception_notification 2-3-stable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,3 +26,6 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+
+ExceptionNotification::Notifier.exception_recipients = "dana@agilestyle.com"
+ExceptionNotification::Notifier.sender_address = %("Application Error" <error@aaastriping.com>)
