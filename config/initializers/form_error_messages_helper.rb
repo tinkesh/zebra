ad0fa4ddaa91af107b12a683e9bf9a9800ca1ca1
Ildar Abdulin <ildar@agilestyle.com>
2013-06-11 12:16:15 -0600

    Changed form.error_messages

--- config/initializers/form_error_messages_helper.rb
+++ config/initializers/form_error_messages_helper.rb
@@ -2,6 +2,7 @@ class ActionView::Helpers::FormBuilder
   def error_messages
     return unless object.respond_to?(:errors) && object.errors.any?
 
-    object.errors.full_messages.map { |message| @template.content_tag(:div, message, :class => 'error') }.join('').html_safe
+    messages = object.errors.full_messages.join('<br>')
+    return @template.content_tag(:div, messages.html_safe, :class => 'error')
   end
 end
