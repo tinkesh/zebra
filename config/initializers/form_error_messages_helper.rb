4bb5dfd1966a4b9fd09d3a9f2a8c59664e08fe22
Ildar Abdulin <ildar@agilestyle.com>
2013-06-14 12:02:36 -0600

    Changed form.errors helper to not display duplicate error messages

--- config/initializers/form_error_messages_helper.rb
+++ config/initializers/form_error_messages_helper.rb
@@ -2,7 +2,7 @@ class ActionView::Helpers::FormBuilder
   def error_messages
     return unless object.respond_to?(:errors) && object.errors.any?
 
-    messages = object.errors.full_messages.join('<br>')
+    messages = object.errors.full_messages.uniq.join('<br>')
     return @template.content_tag(:div, messages.html_safe, :class => 'error')
   end
 end
