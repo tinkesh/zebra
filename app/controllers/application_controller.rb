cf9bf254901e7eb383a81290ead6987150c6520d
Ildar Abdulin <ildar@agilestyle.com>
2013-06-27 11:58:21 -0600

    Deleted "you must be logged out" error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,7 +52,6 @@ private
   def require_no_user
     if current_user
       store_location
-      flash[:notice] = "You must be logged out to access this page"
       redirect_to private_home_url
       return false
     end
