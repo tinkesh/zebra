c94f5c91d76cb1e179c6e78c2dd00790de96107f
Dana Janssen <dana@agilestyle.com>
2013-08-05 17:26:53 -0600

    Fix migrations to work off latest aaastriping pg dump

--- db/migrate/20130613201322_create_comments.rb
+++ db/migrate/20130613201322_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def change
-    create_table :comments do |t|
+    create_table :comments, :force => true do |t|
       t.text :text
       t.integer :user_id
       t.integer :job_id
--- db/migrate/20130705181150_create_daily_report.rb
+++ db/migrate/20130705181150_create_daily_report.rb
@@ -1,11 +1,11 @@
 class CreateDailyReport < ActiveRecord::Migration
   def change
-    create_table :daily_reports do |t|
-      t.integer :user_id
-      t.boolean :loaded, :default => false
-      t.boolean :painted, :default => false
-
-      t.timestamps
-    end
+#    create_table :daily_reports do |t|
+#      t.integer :user_id
+#      t.boolean :loaded, :default => false
+#      t.boolean :painted, :default => false
+#
+#      t.timestamps
+#    end
   end
 end
