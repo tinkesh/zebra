e7bea3ec86bc75852349ed042cab0be5e0717787
Matt Riemer <matthew@agilestyle.com>
2013-09-11 11:16:59 -0600

    update TimeEntry even if there is not a TimeSheet with it

--- app/controllers/private/time_entries_controller.rb
+++ app/controllers/private/time_entries_controller.rb
@@ -11,12 +11,16 @@ class Private::TimeEntriesController < ApplicationController
 
   def update
     @time_entry = TimeEntry.find(params[:id])
-    @time_sheet = TimeSheet.find(@time_entry.time_sheet_id)
+    @time_sheet = TimeSheet.where(:id => @time_entry.time_sheet_id).first
     params[:time_entry][:clock_in] = Time.zone.parse(params[:time_entry][:clock_in])
     params[:time_entry][:clock_out] = Time.zone.parse(params[:time_entry][:clock_out])
     if @time_entry.update_attributes(params[:time_entry])
       flash[:notice] = "Time Entry updated!"
-      redirect_to edit_private_time_sheet_url(@time_sheet)
+      if @time_sheet.present?
+        redirect_to edit_private_time_sheet_url(@time_sheet)
+      else
+        redirect_to report_user_time_path(@time_entry.user_id)
+      end
     else
       render :action => :edit
     end
