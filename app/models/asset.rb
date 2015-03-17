6c2c1d63af38d75325d4ef89b4e38157fc3e0f29
Vlad Lazu <vlad.lazu@gmail.com>
2015-03-17 21:06:09 +0200

    Set link to attachment in jobs and equipment

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,7 +2,7 @@ class Asset < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :attachable, polymorphic: true
   has_attached_file :image, styles: {small: '100x100>'},
-                    url: "/public/system/:id/:style/:basename.:extension",
+                    url: "/system/:id/:style/:basename.:extension",
                     path: ":rails_root/public/system/:id/:style/:basename.:extension"
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
 end
