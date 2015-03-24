7ee8f01480c7f0cee506ba8ff691792cdeb788b5
Vlad Lazu <vlad.lazu@gmail.com>
2015-03-24 14:21:04 +0200

    Update job completed_on and started_on then drag calendar events

--- app/controllers/api/crews_controller.rb
+++ app/controllers/api/crews_controller.rb
@@ -16,16 +16,20 @@ class Api::CrewsController < ApplicationController
     @event = Event.find_by_id(params[:id])
 
     if @event.nil?
-      @job = Job.find(params[:id])
-      @event = @job.events.new(params[:event])
-      @event.name = @job.name
+      job = Job.find_by_id(params[:id])
+      @event = job.events.new(params[:event])
+      @event.name = job.name
       @event.save
     else
       @event.update_attributes(params[:event])
+      job = Job.find_by_id(@event.eventable_id)
     end
 
-
-    #@job.update_attributes(params[:job])
+    if job
+      job.started_on = job.events.order('started_on').first.started_on
+      job.completed_on = job.events.order('completed_on').last.completed_on
+      job.save
+    end
 
     respond_to do |format|
       format.json { render json: @event, root: false }
