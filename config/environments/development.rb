41e06380da3d2a82a7eeedc94f7270d5da561e79
Vlad Lazu <vlad.lazu@gmail.com>
2015-04-06 19:59:02 +0300

    Add region and host_name for amazon uploads storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,8 @@ AaaStriping::Application.configure do
           :bucket => ENV['S3_BUCKET_NAME'],
           :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-          :region => 'us-east-1'
-      }
+          :region => 'oregon'
+      },
+      :s3_host_name => 's3-us-west-2.amazonaws.com'
   }
 end
