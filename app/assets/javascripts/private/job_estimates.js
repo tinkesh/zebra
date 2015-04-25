651dc31f688f428ff8a73dfc876144785b81866b
Rosca Sergiu <rmagnum2002@gmail.com>
2015-04-25 20:25:04 +0300

    fixed invoicing tool js, re-factoring, added validations

--- app/assets/javascripts/private/job_estimates.js
+++ app/assets/javascripts/private/job_estimates.js
@@ -1,6 +1,32 @@
+jQuery(document).ready(function() {
+  updateTable();
+  jQuery('tr.item input').change(function(event) {
+    updateTable();
+  });
+});
+
 jQuery(function () {
   jQuery(".job_estimate").nestedFields({
     itemSelector: ".item",
-    containerSelector: "tbody"
+    containerSelector: "tbody",
+    afterInsert: function(item) {
+      jQuery('tr.item input').change(function(event) {
+        updateTable();
+      });
+    }
   })
 });
+
+function updateTable() {
+  var sum = 0;
+  jQuery("#table_item tbody tr").each(function(){
+   el1 = Number(jQuery(this).find("td.quantity").find('input').val()) * Number(jQuery(this).find("td.price").find('input').val())
+   td = jQuery(this).find("td").eq(3)
+   td.find('input').val(el1);
+   td.find('div.total-price').html(el1);
+   sum += Number(td.find('input').val());
+  });
+  jQuery('.total_amount').html(sum)
+  jQuery('#job_estimate_total_amount').val(sum)
+};
+
--- app/models/estimate_item.rb
+++ app/models/estimate_item.rb
@@ -1,3 +1,5 @@
 class EstimateItem < ActiveRecord::Base
   belongs_to :job_estimate
+
+  validates :title, :quantity, :price, presence: true
 end
