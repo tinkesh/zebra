7a263dfe0b510833d073dcf62b01ed85a721739a
Uma Mahesh <umamaheshvarma@gmail.com>
2016-06-30 15:03:44 +0530

    foreman time sheet - added more value lunch break option

--- app/controllers/private/time_sheets_controller.rb
+++ app/controllers/private/time_sheets_controller.rb
@@ -104,7 +104,7 @@ private
     if current_user.role_symbols.include?(:admin) || current_user.role_symbols.include?(:office)
       @lunch_selections = [0, 30, 45, 60, 75, 90, 105, 120, 150, 180, 210, 240, 270, 300]
     else
-      @lunch_selections = [30, 45, 60, 75, 90, 105, 120]
+      @lunch_selections = [30, 45, 60, 75, 90, 105, 120, 150, 180, 210, 240, 270, 300]
     end
   end
 
